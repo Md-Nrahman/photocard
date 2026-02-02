08d41cdea893f3a9ef7c86e30d9d5146c986595a
Md-Nrahman <md.naimur.rahman.naim@gmail.com>
2026-02-02 12:45:16 +0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://md-nrahman.github.io/", // This must match your repo name
+  base: "https://md-nrahman.github.io/photocard", // This must match your repo name
 });
